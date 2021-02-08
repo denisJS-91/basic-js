67ddcd7d422bee69e17d2711bcae0789c60cb68f
denisJS-91 <77659339+denisJS-91@users.noreply.github.com>
2021-02-08 20:31:13 +0300

    feat:add code carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof(sampleActivity) !== 'string') {
+		return false;
+	} else if (Number.isNaN(parseFloat(sampleActivity))) {
+		return false;
+	} else if (+sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY) {
+		return false;
+	}
+	return Math.ceil(Math.abs( Math.log((parseFloat(sampleActivity)) / MODERN_ACTIVITY) / (-693 / 1000) ) * HALF_LIFE_PERIOD);
 };
