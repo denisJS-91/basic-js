5b1754fb908986598f842f01d4f473f2622b4f5d
denisJS-91 <77659339+denisJS-91@users.noreply.github.com>
2021-02-08 20:46:09 +0300

    feat:added code vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,39 @@
 const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+	constructor(type = true) {
+		this.type = type;
+	}
+  encrypt(message, key) {
+		if (!message || !key) throw Error('Missing argument');		
+		let keyString = key.repeat( Math.ceil( message.length / key.length ) ).toUpperCase(),
+				messageString = message.toUpperCase(),
+				cipherString = new Array(messageString.length);
+		for (let i = 0, j = 0; i < messageString.length; i++, j++) {
+			if (messageString.charCodeAt(i) >= 65 
+					&& messageString.charCodeAt(i) <= 90) {
+			cipherString[i] = String.fromCharCode( ((messageString.charCodeAt(i) + keyString.charCodeAt(j) - 130) % 26) + 65 );
+			} else {
+				cipherString[i] = messageString[i];
+				j--;
+			}
+		};
+    return this.type ? cipherString.join('').toUpperCase() : cipherString.reverse().join('').toUpperCase();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+		if (!message || !key) throw Error('Missing argument');
+		let keyString = key.repeat( Math.ceil( message.length / key.length ) ).toUpperCase(),
+				messageString = message.toUpperCase(),
+				cipherString = new Array(messageString.length);
+		for (let i = 0, j = 0; i < messageString.length; i++, j++) {
+			if (messageString.charCodeAt(i) >= 65 
+					&& messageString.charCodeAt(i) <= 90) {
+			cipherString[i] = String.fromCharCode( ((messageString.charCodeAt(i) + 26 - keyString.charCodeAt(j)) % 26) + 65 );
+			} else {
+				cipherString[i] = messageString[i];
+				j--;
+			}
+		};
+		return this.type ? cipherString.join('').toUpperCase() : cipherString.reverse().join('').toUpperCase();
   }
 }
 
