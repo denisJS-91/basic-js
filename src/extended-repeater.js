415b5951c33b6567a78c1d484cb01531e30f392c
denisJS-91 <77659339+denisJS-91@users.noreply.github.com>
2021-02-08 20:38:32 +0300

    feat:change let result

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+
+  if (typeof options['separator'] === "undefined") options['separator'] = '+';
+  if (typeof options['addition'] === "undefined") options['addition'] = "";
+  if (!options['repeatTimes']) {
+    return str + options['addition'];
+  } else {
+    result = (str + (options['addition'] + options['additionSeparator']).repeat(options['additionRepeatTimes'] - 1)
+      + options['addition'] + options['separator']).repeat(options['repeatTimes']);
+  }
+
+  return result.substring(0, result.length - options['separator'].length);
 };
   
